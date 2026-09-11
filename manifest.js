06004433fa454e9f107a2702c96529e64796cb56
worshipbkc <phallakhum6@gmail.com>
2026-09-11 10:35:19 +0700

    manifest.json

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "W.BKC",
+  "short_name": "W.BKC",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "icons": [
+    {
+      "src": "icon.jpg",
+      "sizes": "512x512",
+      "type": "image/jpeg"
+    }
+  ]
+}
